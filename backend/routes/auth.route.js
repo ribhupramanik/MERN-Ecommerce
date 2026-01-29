35ead09663f5bd4d06b8555d05e6b6aed2437c52
ribhupramanik <ribhu02@gmail.com>
2026-01-29 20:07:07 +0530

    Add authentication routes and update server configuration

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/signup", (req, res) => {
+  res.send("Sign Up route called");
+});
+
+router.get("/login", (req, res)=> {
+    res.send("login route called")
+})
+
+router.get("/logout", (req, res)=> {
+    res.send("logout route called")
+})
+
+export default router;
