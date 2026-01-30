92b3c464e99819cbe49c6008c8d0a035d560b9d8
ribhupramanik <ribhu02@gmail.com>
2026-01-30 17:49:13 +0530

    Setup User Signup API

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -53,7 +53,7 @@ userSchema.pre("save", async function (next) {
   }
 });
 
-userSchema.methods.comparePassword -
+userSchema.methods.comparePassword =
   async function (password) {
     return bcrypt.compare(password, this.password);
   };
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ dotenv.config()
 const app = express()
 const PORT = (process.env.PORT)
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
