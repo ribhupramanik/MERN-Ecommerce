edbce9786378c6383196c175174062d76b79b05a
ribhupramanik <ribhu02@gmail.com>
2026-01-30 18:05:38 +0530

    Refactor authentication controller and routes to improve user signup logic and change HTTP methods to POST

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,14 +2,23 @@ import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
-  const userExists = await User.findOne({ email });
 
-  if (userExists) {
-    return res.status(400).json({ success: false, message: "User already exists" });
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+    const user = await User.create({ name, email, password });
+
+    res
+      .status(200)
+      .json({ success: true, message: "User created successfully", user });
+  } catch (error) {
+    res.status(500).json(error.message);
   }
-  const user = await User.create({ name, email, password });
-
-  res.status(200).json({ success: true, message: "User created successfully",user });
 };
 
 export const login = async (req, res) => {
