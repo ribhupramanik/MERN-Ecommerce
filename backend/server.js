61b9f229a98339620afd985aad7a3801749674fe
ribhupramanik <ribhu02@gmail.com>
2026-01-30 17:04:10 +0530

    Implement user authentication and database connection

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 
 import authRoutes from "./routes/auth.route.js"
+import { connectDb } from "./lib/db.js"
 
 dotenv.config()
 
@@ -12,4 +13,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`)
+    connectDb();
 })
