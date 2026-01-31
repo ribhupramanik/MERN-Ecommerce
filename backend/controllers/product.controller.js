bd2a75d985cb2d92a543931782dcffeacaf6ff13
ribhupramanik <ribhu02@gmail.com>
2026-01-31 22:38:03 +0530

    Created the GetProductsByCategory Route and Controller

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -108,3 +108,14 @@ export const getRecomendedProducts = async (req, res) => {
         res.status(500).json({ message: "Server error", error: error.message });
     }
 }
+
+export const getProductsByCategory = async (req,res) => {
+    const {category} = req.params
+    try {
+        const products = await Product.find({category})
+        res.json(products)
+    } catch (error) {
+        console.log("Error in getProductsByCategory controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -4,6 +4,7 @@ import {
   deleteProduct,
   getAllProducts,
   getFeaturedProducts,
+  getProductsByCategory,
   getRecomendedProducts,
 } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
@@ -12,6 +13,7 @@ const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.get("/category/:category", getProductsByCategory);
 router.get("/recomendations", protectRoute, getRecomendedProducts);
 router.post("/", protectRoute, adminRoute, createProduct);
 router.delete("/:id", protectRoute, adminRoute, deleteProduct);
