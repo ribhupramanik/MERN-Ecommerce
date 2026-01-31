a7a02180573a5cc2f005218ecc9d742b8a281c7c
ribhupramanik <ribhu02@gmail.com>
2026-01-31 22:34:13 +0530

    Created getRecomendedProducts route and controller

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,14 +1,19 @@
-import express from "express"
-import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js"
-import { adminRoute, protectRoute } from "../middleware/auth.middleware.js"
+import express from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getFeaturedProducts,
+  getRecomendedProducts,
+} from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
+router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.get("/recomendations", protectRoute, getRecomendedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
-router.get("/", protectRoute, adminRoute, getAllProducts)
-router.get("/featured", getFeaturedProducts)
-router.post("/", protectRoute, adminRoute, createProduct)
-router.delete("/:id", protectRoute, adminRoute, deleteProduct)
-
-export default router
-
+export default router;
