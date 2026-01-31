57ae8a59e79b388e0dfde6eb6dcf6eea2abfeb0f
ribhupramanik <ribhu02@gmail.com>
2026-01-31 21:53:10 +0530

    Add createProduct controller and integrate Cloudinary for image uploads

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js"
 
@@ -30,5 +31,29 @@ export const getFeaturedProducts = async (req, res) => {
     } catch (error) {
         console.log("Error in getFeaturedProducts controller", error.message)
         res.status(500).json({message: "Server error", error: error.message})
+    }
+}
+
+export const createProduct = async (req, res) => {
+    try {
+        const {name, description, price, image, category} = req.body
+
+        let cloudinaryResponse = null
+        
+        if(image){
+            await cloudinary.uploader.upload(image, {folder:"products"})
+        }
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "",
+            category
+        })
+
+        res.status(200).json(product)
+    } catch (error) {
+        console.log("Error in createProduct controller", error.message)
+        res.status(500).json({message: "Server error", error:error.message})
     }
 }
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,12 +1,13 @@
 import express from "express"
-import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js"
+import { createProduct, getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js"
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
 
 router.get("/", protectRoute, adminRoute, getAllProducts)
-router.get("/", getFeaturedProducts)
+router.get("/featured", getFeaturedProducts)
+router.post("/", protectRoute, adminRoute, createProduct)
 
 export default router
 
