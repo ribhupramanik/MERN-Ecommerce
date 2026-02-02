4e8a63368829794b6451241bffdd2dcc42bd0fd2
ribhupramanik <ribhu02@gmail.com>
2026-02-02 14:39:55 +0530

    Add functionality to create a new coupon for orders over 200

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -50,7 +50,10 @@ export const createCheckoutSession = async (req, res) => {
         couponCode:couponCode || ""
       }
     })
-
+    if(totalAmount >= 20000){
+      await createNewCoupon(req.user._id)
+    }
+    res.status(200).json({id:session.id, totalAmount: totalAmount /100})
   } catch (error) {
     
   }
@@ -64,3 +67,14 @@ async function createStripeCoupon(discountPercentage){
   return coupon.id
 }
 
+async function createNewCoupon(userId){
+  const newCoupon = new Coupon({
+    code: "GIFT" + Math.random().toString(36).substring(2,8).toUpperCase(),
+    discountPercentage:10,
+    expirationDate: new Date(Date.now() + 30*24*60*60*1000),
+    userId:userId
+  })
+  await newCoupon.save()
+
+  return newCoupon
+}
