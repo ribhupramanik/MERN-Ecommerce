b5705d956d69c9b85f4203099387a94649bbd656
ribhupramanik <ribhu02@gmail.com>
2026-02-02 21:48:40 +0530

    Add analytics controller and route for fetching analytics data

---
+++ backend/routes/analytics.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
+import { getAnalytics } from "../controllers/analytics.controller.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, adminRoute, async (req, res) => {
+  try {
+    const analyticsData = await getAnalyticsData();
+  } catch (error) {}
+});
+
+export default router;
