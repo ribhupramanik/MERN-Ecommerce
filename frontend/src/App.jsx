eebe3840ade52ebc6a963bbf9ba0030921027247
ribhupramanik <ribhu02@gmail.com>
2026-02-03 20:29:19 +0530

    add routing and new pages for Home, Login, and Sign Up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  
-
-  return <div className="text-red-500">Hello World</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="login" element={<LoginPage />} />
+    </Routes>
+  );
 }
 
 export default App;
