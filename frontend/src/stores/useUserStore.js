a562c2ca56105d80cfc53ae97f98807d0c92ff31
ribhupramanik <ribhu02@gmail.com>
2026-02-03 23:15:34 +0530

    Fixed CORS error and added toast success message when loging in and signin up

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -18,6 +18,7 @@ export const useUserStore = create((set, get) => ({
 		try {
 			const res = await axios.post("/auth/signup", { name, email, password });
 			set({ user: res.data, loading: false });
+			toast.success("User signed Up Successfully")
 		} catch (error) {
 			set({ loading: false });
 			toast.error(error.response.data.message || "An error occurred");
@@ -30,6 +31,7 @@ export const useUserStore = create((set, get) => ({
 			const res = await axios.post("/auth/login", { email, password });
 
 			set({ user: res.data, loading: false });
+			toast.success('Login Successful')
 		} catch (error) {
 			set({ loading: false });
 			toast.error(error.response.data.message || "An error occurred");
