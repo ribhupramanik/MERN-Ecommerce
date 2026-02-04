4a5d6a6aac80d019673b282608e25cd8084d6b8a
ribhupramanik <ribhu02@gmail.com>
2026-02-04 19:59:49 +0530

    Created the loading spinner component and added auth checks to login and signup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import Navbar from "./components/Navbar";
 import { Toaster } from "react-hot-toast";
+import { useUserStore } from "./stores/useUserStore";
+import { useEffect } from "react";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
+  const {user, checkAuth, checkingAuth} = useUserStore()
+
+  useEffect(() => {
+    checkAuth()
+  },[checkAuth])
+
+  if (checkingAuth) return <LoadingSpinner />;
+
   return (
     <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
       <div className="absolute inset-0 overflow-hidden">
@@ -18,8 +29,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="login" element={<LoginPage />} />
+          <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to ='/'/> } />
+          <Route path="login" element={! user ? <LoginPage /> : <Navigate to ='/'/> } />
         </Routes>
       </div>
       <Toaster />
