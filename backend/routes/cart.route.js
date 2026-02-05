0b1b12f1da5e4de1e52dea897f744f87ac39230c
ribhupramanik <ribhu02@gmail.com>
2026-02-05 21:46:55 +0530

    Created the Cart Page

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -7,6 +7,6 @@ const router = express.Router()
 router.get("/", protectRoute, getCartProducts)
 router.post("/", protectRoute,addToCart)
 router.delete("/", protectRoute, removeAllFromCart)
-router.put("/", protectRoute, updateQuantity)
+router.put("/:id", protectRoute, updateQuantity)
 
 export default router
