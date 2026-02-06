96aac18687eb328310ff443a3928a1d60cb93f32
ribhupramanik <ribhu02@gmail.com>
2026-02-06 22:08:59 +0530

    Stripe payment connected to let users pay with stripe

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -3,16 +3,28 @@ import {motion} from 'framer-motion'
 import { useCartStore } from '../stores/useCartStore'
 import { MoveRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
-
-
+import axios from '../lib/axios';
 
 const OrderSummary = () => {
-  const {total, subtotal, coupon, isCouponApplied} = useCartStore();
+  const {total, subtotal, coupon, isCouponApplied, cart} = useCartStore();
   const savings = subtotal-total
   const formattedSubtotal = subtotal.toFixed(2)
   const formattedTotal = total.toFixed(2)
   const totalSavings = savings.toFixed(2)
 
+  	const handlePayment = async () => {
+      try {
+        const res = await axios.post("/payments/create-checkout-session", {
+          products: cart,
+          couponCode: coupon ? coupon.code : null,
+        });
+
+        
+        window.location.href = res.data.id;
+      } catch (error) {
+        console.error("Checkout error:", error);
+      }
+    };
 
   
   return (
@@ -28,12 +40,12 @@ const OrderSummary = () => {
           <div className='space-y-2'>
             <dl className='flex items-center justify-between gap-4'>
               <dt className='text-base font-normal text-gray-300'>Original price</dt>
-              <dd className='text-base font-medium text-white'>${formattedSubtotal}</dd>
+              <dd className='text-base font-medium text-white'>₹{formattedSubtotal}</dd>
 					  </dl>
             {savings > 0 && (
               <dl className='flex items-center justify-between gap-4'>
                 <dt className='text-base font-normal text-gray-300'>Savings</dt>
-                <dd className='text-base font-medium text-emerald-400'>-${formattedSavings}</dd>
+                <dd className='text-base font-medium text-emerald-400'>-₹{formattedSavings}</dd>
               </dl>
             )}
             
@@ -46,14 +58,14 @@ const OrderSummary = () => {
 
             <dl className='flex items-center justify-between gap-4 border-t border-gray-600 pt-2'>
               <dt className='text-base font-bold text-white'>Total</dt>
-              <dd className='text-base font-bold text-emerald-400'>${formattedTotal}</dd>
+              <dd className='text-base font-bold text-emerald-400'>₹{formattedTotal}</dd>
             </dl>
           </div>
           	<motion.button
               className='flex w-full items-center justify-center rounded-lg bg-emerald-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300'
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
-              
+              onClick={handlePayment}
             >
               Proceed to Checkout
             </motion.button>
