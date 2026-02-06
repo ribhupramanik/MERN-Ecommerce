92544a45410534dd220599abd7bad56f9489b3a5
ribhupramanik <ribhu02@gmail.com>
2026-02-06 22:20:03 +0530

    Created the purchase success page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import AdminPage from "./pages/AdminPage";
 import CategoryPage from "./pages/CategoryPage";
 import CartPage from "./pages/CartPage";
 import { useCartStore } from "./stores/useCartStore";
+import PurchaseSuccessPage from "./pages/PurchaseSuccessPage";
 
 function App() {
   const {user, checkAuth, checkingAuth} = useUserStore()
@@ -45,6 +46,7 @@ function App() {
           <Route path="/secret-dashboard" element={user?.role === 'admin' ? <AdminPage /> : <Navigate to ='/login'/> } />
           <Route path="/category/:category" element={<CategoryPage /> } />
           <Route path="/cart" element={user ? <CartPage /> : <Navigate to='/login'/> } />
+          <Route path="/purchase-success" element={user ? <PurchaseSuccessPage /> : <Navigate to='/login'/> } />
         </Routes>
       </div>
       <Toaster />
