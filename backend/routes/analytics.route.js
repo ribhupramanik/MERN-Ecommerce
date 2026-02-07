4dba0ab5b5c1e25efec8ca20ba795b67fcfc4705
ribhupramanik <ribhu02@gmail.com>
2026-02-07 21:59:21 +0530

    Made changes to fetch correct analytics data

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -8,8 +8,8 @@ router.get("/", protectRoute, adminRoute, async (req, res) => {
   try {
     const analyticsData = await getAnalyticsData();
 
-    const startDate = new Date()
-    const endDate = new Date(endDate.getTime() -7 * 24 * 60 * 60 * 1000)
+    const endDate = new Date();
+    const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
 
     const dailySalesData = await getDailySalesData(startDate, endDate)
 
