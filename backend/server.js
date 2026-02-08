bcacfad3bcca883cecc50f39c9587e4e17219b91
ribhupramanik <ribhu02@gmail.com>
2026-02-08 22:21:07 +0530

    Made changes to deploy application using render

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js"
@@ -15,6 +16,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve()
+
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -25,6 +28,13 @@ app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes)
 app.use("/api/payments", paymentRoutes)
 app.use("/api/analytics", analyticsRoutes)
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
