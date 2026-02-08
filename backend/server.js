882b3ca55f854840f627a2383fe9e07ab331f5a5
ribhupramanik <ribhu02@gmail.com>
2026-02-08 22:45:46 +0530

    Made changes to ensure that site can be deployed using render

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,10 @@ app.use("/api/payments", paymentRoutes)
 app.use("/api/analytics", analyticsRoutes)
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")))
-  app.get("*", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  })
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
